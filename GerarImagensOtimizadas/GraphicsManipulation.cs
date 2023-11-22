4fbf207f1468e40c8081f795d0dc2d306636f806
Charles Silva <charles.augusto.silva@gmail.com>
2023-11-22 06:17:57 -0300

    Init

---
+++ GerarImagensOtimizadas/GraphicsManipulation.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Linq;
+//using System.Text;
+using System.Threading.Tasks;
+
+namespace GerarImagensOtimizadas
+{
+    public class GraphicsManipulation
+    {
+        //public ImageCodecInfo GetEncoderInfo(string mimeType)
+        //{
+        //    ImageCodecInfo[] encoders = ImageCodecInfo.GetImageEncoders();
+        //    foreach (ImageCodecInfo item in encoders)
+        //        if (item.MimeType == mimeType)
+        //            return item;
+        //    throw new Exception("ImageCodecInfo not found");
+        //}
+
+        public Stream SetJpgImageQuality(Stream stream, byte compression)
+        {
+            Image image = Image.FromStream(stream);
+            //EncoderParameters eps = new EncoderParameters();
+            //ImageCodecInfo ici = this.GetEncoderInfo("image/jpeg");
+
+            //eps.Param[0] = new EncoderParameter(Encoder.Quality, compression);
+
+            image.Save(stream, ImageFormat.Jpeg); //, ici, eps);
+
+            return stream;
+        }
+
+        public Stream CreateThumbnailProportional(Stream stream, int width, int height)
+        {
+            int nWidth, nHeight;
+            double pct;
+            Bitmap bitmap = new Bitmap(stream);
+            Image image = Image.FromStream(stream);
+
+            if (bitmap.Size.Width > width)
+            {
+                pct = (double)(image.Size.Width - width) / (double)image.Size.Width;
+                // decrease height proportional
+                nHeight = (int)(image.Size.Height * (1 - pct));
+                nWidth = width;
+
+                stream = this.CreateThumbnail(stream, nWidth, nHeight);
+
+                bitmap = (Bitmap)Image.FromStream(stream);
+            }
+
+            if (bitmap.Size.Height > height)
+            {
+                pct = (double)(image.Size.Height - height) / (double)image.Size.Height;
+                // decrease width proportional
+                nHeight = height;
+                nWidth = (int)(image.Size.Width * (1 - pct));
+
+                stream = this.CreateThumbnail(stream, nWidth, nHeight);
+            }
+
+            return stream;
+        }
+
+        public System.IO.Stream CreateThumbnail(System.IO.Stream st, double wid, double hght)
+        {
+            var ms = new MemoryStream();
+
+            using (Image imagesize = Image.FromStream(st)) // using (Image imagesize = Image.FromFile(f.FullName))
+            {
+                using (Bitmap bitmapNew = new Bitmap(imagesize))
+                {
+                    double maxWidth = wid;
+                    double maxHeight = hght;
+                    int w = imagesize.Width;
+                    int h = imagesize.Height;
+                    // Longest and shortest dimension 
+                    int longestDimension = (w > h) ? w : h;
+                    int shortestDimension = (w < h) ? w : h;
+                    // propotionality  
+                    float factor = ((float)longestDimension) / shortestDimension;
+                    // default width is greater than height    
+                    double newWidth = maxWidth;
+                    double newHeight = maxWidth / factor;
+                    // if height greater than width recalculate  
+                    if (w < h)
+                    {
+                        newWidth = maxHeight / factor;
+                        newHeight = maxHeight;
+                    }
+
+                    bitmapNew.GetThumbnailImage((int)newWidth, (int)newHeight, () => false, IntPtr.Zero)
+                        .Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+            }
+
+            return ms;
+        }
+
+    }
+
+}
---
+++ GerarImagensOtimizadas/Program.cs
@@ -0,0 +1,123 @@
+﻿
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace GerarImagensOtimizadas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderWithImagesToResize = @"C:\dados\git\lixo\GerarImagensOtimizadas\bin\Debug\imagesToBeResized";
+            string folderToSaveResizedImages = @"C:\dados\git\lixo\GerarImagensOtimizadas\bin\Debug\imagesResized";
+
+            // If there is any TIFF image in the folder, convert it to JPEG
+            foreach (var file in Directory.GetFiles(folderWithImagesToResize, "*.tif"))
+            {
+                ConvertTifToJpg(file, Path.Combine(folderWithImagesToResize, Path.GetFileNameWithoutExtension(file) + ".jpg"));
+            }
+
+            // Resize all JPEG images in the folder
+            ProcessarJPGImages(folderWithImagesToResize, folderToSaveResizedImages, 800, 600);
+
+            Console.ReadKey();
+        }
+
+        static void ProcessarJPGImages(string folderSource, string folderDestination, int maxWidth, int maxHeight)
+        {
+            foreach (var file in Directory.GetFiles(folderSource, "*.jpg"))
+            {
+                //var image = ResizeImageProportionally(file, maxWidth, maxHeight);
+                //image.Save(Path.Combine(folderDestination, Path.GetFileName(file)));
+
+                var gm = new GraphicsManipulation();
+
+                // Read file as stream
+                var ms = new MemoryStream(File.ReadAllBytes(file));
+                var stream = gm.CreateThumbnailProportional(ms, 1024, 800);
+                System.IO.File.WriteAllBytes(
+                    Path.Combine(folderDestination, Path.GetFileName(file)), 
+                    ConvertStreamToBytes(stream));
+            }
+        }
+
+        public static byte[] ConvertStreamToBytes(System.IO.Stream stream)
+        {
+            byte[] content = new byte[stream.Length];
+            stream.Position = 0;
+            stream.Read(content, 0, int.Parse(stream.Length.ToString()));
+            return content;
+        }
+
+        public static void ConvertTifToJpg(string tifFilePath, string jpgFilePath)
+        {
+            try
+            {
+                // Load the TIFF image
+                Bitmap originalImage = new Bitmap(tifFilePath);
+
+                // Save the TIFF image as a JPEG image
+                originalImage.Save(jpgFilePath, ImageFormat.Jpeg);
+
+                // Dispose of the original image
+                originalImage.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error converting TIFF to JPEG: " + ex.Message);
+            }
+        }
+
+        public static Bitmap ResizeImageProportionally(string filePath, int maxWidth, int maxHeight)
+        {
+            // Load the image from the file path
+            Bitmap originalImage = new Bitmap(filePath);
+
+            // Calculate the new dimensions maintaining the aspect ratio
+            double ratio = (double)originalImage.Width / (double)originalImage.Height;
+            int newWidth, newHeight;
+
+            if (maxWidth > 0 && maxHeight > 0)
+            {
+                if (originalImage.Width > originalImage.Height)
+                {
+                    newWidth = maxWidth;
+                    newHeight = (int)(maxWidth * ratio);
+                }
+                else
+                {
+                    newHeight = maxHeight;
+                    newWidth = (int)(maxHeight * ratio);
+                }
+            }
+            else if (maxWidth > 0)
+            {
+                newWidth = maxWidth;
+                newHeight = (int)(maxWidth * ratio);
+            }
+            else if (maxHeight > 0)
+            {
+                newHeight = maxHeight;
+                newWidth = (int)(maxHeight * ratio);
+            }
+            else
+            {
+                throw new ArgumentException("Either maxWidth or maxHeight must be greater than 0.");
+            }
+
+            // Resize the image to the calculated dimensions
+            Bitmap resizedImage = new Bitmap(newWidth, newHeight);
+            using (Graphics g = Graphics.FromImage(resizedImage))
+            {
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.DrawImage(originalImage, 0, 0, newWidth, newHeight);
+            }
+
+            // Dispose of the original image
+            originalImage.Dispose();
+
+            return resizedImage;
+        }
+    }
+}
